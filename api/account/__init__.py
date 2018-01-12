68e72cde2eaff72a871e2efabdc96654871cb974
edison <2499090390@qq.com>
2018-01-12 12:24:51 +0800

    fix

--- api/account/__init__.py
+++ api/account/__init__.py
@@ -1,1 +1,9 @@
 # _*_ coding:utf-8 _*_
+
+import requests
+appId = "wxe02491f98cc727ad"
+sessionKey = "0818gUwY0Nou822fbiuY0maKwY08gUwh"
+url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=ddf03c948cfe2610dd8d1ae125b212ea&' \
+      'code=%s&grant_type=authorization_code' % (appId, sessionKey)
+res = requests.get(url)
+print res.content
