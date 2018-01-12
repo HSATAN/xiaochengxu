13e3abcca139730b146cc0bcea9f205572e8ac75
edison <2499090390@qq.com>
2018-01-12 12:31:10 +0800

    fix

--- api/weixin/auth.py
+++ api/weixin/auth.py
@@ -6,6 +6,8 @@ import random
 import logging
 from config.default import appId
 from WXBizDataCrypt import WXBizDataCrypt
+import json
+import requests
 
 
 class AuthWeiXin(BaseResource):
@@ -59,13 +61,12 @@ class WXRunData(BaseResource):
 
         iv = request.args.get('iv')[0]
         print('iv=%s' % iv)
-        import requests
-
         url = 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=ddf03c948cfe2610dd8d1ae125b212ea&' \
               'js_code=%s&grant_type=authorization_code' % (appId, code)
         res = requests.get(url)
         print(res.content)
-        sessionKey = res.content['session_key']
+        content = json.loads(res.content)
+        sessionKey = content['session_key']
         print(sessionKey)
         pc = WXBizDataCrypt(appId, sessionKey)
         print("rundata = %s" % rundata)
@@ -73,15 +74,3 @@ class WXRunData(BaseResource):
         logging.info("nickname %s" % nickName)
         print(nickName)
         return "success"
-
-def main():
-    appId = 'wx4f4bc4dec97d474b'
-    sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
-    encryptedData = 'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew=='
-    iv = 'r7BXXKkLb8qrSNn05n0qiA=='
-
-    pc = WXBizDataCrypt(appId, sessionKey)
-
-    print(pc.decrypt(encryptedData, iv))
-
-main()
