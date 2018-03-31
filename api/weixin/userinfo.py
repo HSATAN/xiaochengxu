30b179344223e1fc179414021951840803d30df1
edison <2499090390@qq.com>
2018-03-31 19:13:32 +0800

    fix

--- api/weixin/userinfo.py
+++ api/weixin/userinfo.py
@@ -20,6 +20,5 @@ class UserInfo(BaseResource):
         openid = request.args.get('openid')[0]
         logging.info("select * from  %s openid='%s' and day=20180329" % (RUN_TABLE, openid))
         data = MysqlDB.run_query("select * from  %s where openid='%s' and runday=20180329" % (RUN_TABLE, openid))
-        if not data:
-            return {'code': 1, 'data': None}
-        return {'code': 0, 'data': data[0]}
+
+        return data[0]
