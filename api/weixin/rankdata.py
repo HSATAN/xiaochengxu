f8fa408c29f33962b725cd6592c59621a8298421
edison <2499090390@qq.com>
2018-03-31 21:04:08 +0800

    fix

--- api/weixin/rankdata.py
+++ api/weixin/rankdata.py
@@ -20,7 +20,7 @@ class RankData(BaseResource):
     def real_POST(self, request):
         openid = request.args.get('openid')[0]
         day = get_day(time.time())
-        logging.info("select * from  %s openid='%s'" % (RUN_TABLE, openid))
+        logging.info("select * from  %s where openid='%s' and runday=%s ORDER BY step DESC" % (RUN_TABLE, openid, day))
         data = MysqlDB.run_query("select * from  %s where openid='%s' and runday=%s ORDER BY step DESC" % (RUN_TABLE, openid, day))
 
         return data
